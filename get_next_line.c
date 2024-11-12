ab2dd828b460b11d6389bfc244cb61cedc7fefe4
dde-lima <daniel.jmendes2@gmail.com>
2024-11-12 18:18:09 -0300

    a

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dde-lima <dde-lima@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 15:55:04 by dde-lima          #+#    #+#             */
-/*   Updated: 2024/11/08 12:25:34 by dde-lima         ###   ########.fr       */
+/*   Updated: 2024/11/12 18:17:02 by dde-lima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ static char *remove_line(char *backup)
 	new_backup = (char *)malloc(sizeof(char) * (ft_strlen(backup) - i + 1));
 	if(!new_backup)
 		return (NULL);
+	i++;
 	while(backup[i])
 		new_backup[j++] = backup[i++];
 	free(backup);
@@ -42,15 +43,15 @@ static char *get_line(char *backup)
 	int i;
 	char *line;
 
-	i = 0;
 	if (!*backup)
 		return (NULL);
 	i = 0;
 	while (backup[i] != '\0' &&  backup[i] != '\n')
 		i++;
-	line = (char *)malloc(sizeof(char) * i + 2);
+	line = (char *)malloc(sizeof(char) * (i + 2));
 	if (!line)
 		return (NULL);
+	i++;
 	ft_strlcpy(line, backup, i + 1);
 	if (backup[i] == '\n')
 		line[i++] = '\n';
@@ -58,12 +59,14 @@ static char *get_line(char *backup)
 	return(line);
 }
 
-static char *read_line(int fd, char *backup)
+static char *read_file(int fd, char *backup)
 {
 	char *buffer;
 	int bytes_read;
 
 	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
 	bytes_read = 1;
 	while (bytes_read > 0 && !ft_strchr(backup, '\n'))
 	{
@@ -86,7 +89,7 @@ char *get_next_line(int fd)
 	char *line;
 	if (fd < 0  || BUFFER_SIZE < 1)
 		return (NULL);
-	backup = read_line(fd, backup);
+	backup = read_file(fd, backup);
 	if(!backup)
 		return (NULL);
 	line = get_line(backup);
@@ -95,9 +98,24 @@ char *get_next_line(int fd)
 }
 
 
-#include <stdio.h>
-int main()
-{
-	 int fd = open("file.txt", O_RDONLY);
-	 get_next_line(fd);
-}
+// #include <stdio.h>
+// int main(void)
+// {
+// 	int fd;
+// 	char *line;
+
+
+// 	fd = open("file.txt", O_RDONLY);
+// 	if (fd < 0)
+// 	{
+// 		perror("Erro ao abrir o arquivo");
+// 		return (1);
+// 	}
+// 	while ((line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("Linha: %s", line);
+// 		free(line);
+// 	}
+// 	return (0);
+// }
+
